265c80260945a7e1b39b726b38aa5ad228e211c7
Rory <owensrory16@gmail.com>
2022-03-02 16:41:23 +0000

    Added the ability to book a meeting from the list of available times which then updates the list of available times by removing the one selected for booking a meeting

--- scheduling_assistant.py
+++ scheduling_assistant.py
@@ -11,28 +11,55 @@ def scheduling_logic():
     # In the test schedules for now a number for example: [9] represents a 1 hour meeting at 9 until 10 which means 10 is still free
 
     # This list is the combination of both schedules
-    test_schedule3 = test_schedule + test_schedule2
+    unavailable_times = test_schedule + test_schedule2
     
     # This list is the sorted combination
-    test_schedule3.sort()
+    unavailable_times.sort()
     
-    # This removes duplicated from the combination list
-    test_schedule3 = list(dict.fromkeys(test_schedule3))
+    # This removes duplicates from the combination list
+    unavailable_times = list(dict.fromkeys(unavailable_times))
     
-    print("These are the times that cannot be used " + str(test_schedule3))
+    print("These are the times that cannot be used " + str(unavailable_times))
     
     
-    # Function to remove times that cannot be used from allowed times list
-    def remove_times(default_allowed_times, test_schedule3):
+    # Function to add times that are not in the list of unavailable times list to a new list of allowed times
+    def remove_times(default_allowed_times, unavailable_times):
         allowed_times = []
         for i in default_allowed_times:
-            if i not in test_schedule3:
+            if i not in unavailable_times:
                 allowed_times.append(i)
         return allowed_times
 
-    allowed_times = remove_times(default_allowed_times, test_schedule3)
+    allowed_times = remove_times(default_allowed_times, unavailable_times)
     
     print("These are the times that can be used to schedule a meeting: " + str(allowed_times))
+    
+    chosen_time=  input("Choose an available time " + str(allowed_times) + ": ")
+    
+    # Determine whether the meeting takes place am or pm and display to user
+    if int(chosen_time) < 13:
+        print("You have booked a meeting for " + str(chosen_time) + ":00 am.")
+    else:
+        print("You have booked a meeting for " + str(chosen_time) + ":00 pm.")
+         
+    
+    
+    # Add the chosen time from the user input to the unavailable times list
+    unavailable_times.append(int(chosen_time))
+    
+    # Function to add the additional time from the user input to a new allowed times list
+    def remove_chosen_times(allowed_times, unavailable_times):
+        allowed_times2 = []
+        for i in allowed_times:
+            if i not in unavailable_times:
+                allowed_times2.append(i)
+        return allowed_times2
+    
+    allowed_times2 = remove_chosen_times(allowed_times, unavailable_times)
+    
+    print("These are the updated available times: "+ str(allowed_times2))
+    
+    
     
 
 
